4283fb08684322f4bc406766f58cf467482d3529
Jorge Armando Medina Rosas <jmedina.externo@coopeuch.cl>
2023-06-29 16:53:27 -0600

    cambios

--- src/main/java/com/nuup/kafkademo/productors/StockPriceChangeGenerator.java
+++ src/main/java/com/nuup/kafkademo/productors/StockPriceChangeGenerator.java
@@ -6,6 +6,6 @@ public class StockPriceChangeGenerator {
     public String generateStockPriceChangeMessage(Double messageId) {
         // Aquí puedes implementar la lógica para generar un mensaje de cambio de precio de una acción de bolsa
         // Puedes incluir detalles como el nombre de la acción, el precio anterior y el nuevo precio, etc.
-        return "valor de accion" + messageId;
+        return "valor de accion - " + messageId;
     }
 }
